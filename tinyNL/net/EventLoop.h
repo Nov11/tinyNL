31b1867007b082c1e2abfec6a1e4dede8a1ce819
Nov11 <529079634@qq.com>
2017-02-22 23:30:43 +0800

    format file

--- tinyNL/net/EventLoop.h
+++ tinyNL/net/EventLoop.h
@@ -16,65 +16,83 @@
 #include <utmpx.h>
 #include <atomic>
 
-namespace tinyNL{
-    namespace net{
+namespace tinyNL {
+    namespace net {
         class Channel;
+
         class Multiplexer;
+
         class TimerQueue;
+
         class Timer;
-        class EventLoop : Noncopyable{
+
+        class EventLoop : Noncopyable {
         public:
             EventLoop();
+
             ~EventLoop();
+
             void loop();
+
             void stop();
 
             bool isInLoopThread() const {
                 return threadId_ == tinyNL::net::CurrentThread::tid();
             }
 
-            void assertInLoopThread(){
-                if(!isInLoopThread()){
+            void assertInLoopThread() {
+                if (!isInLoopThread()) {
                     logAndAbort();
                 }
             }
 
-            void logAndAbort(){
+            void logAndAbort() {
                 std::cerr << "running thread with id:" << tinyNL::net::CurrentThread::tid()
-                                         << " is not the owner of event loop (ptr): "
-                                         << this
-                                         << " which should be "
-                                         << threadId_
-                                         << std::endl;
+                          << " is not the owner of event loop (ptr): "
+                          << this
+                          << " which should be "
+                          << threadId_
+                          << std::endl;
                 _exit(1);
             }
 
-            void updateMultiplexer(Channel* channel);
+            void updateMultiplexer(Channel *channel);
 
-            void runInLoopThread(const std::function<void()>& func);
+            void runInLoopThread(const std::function<void()> &func);
+
             void wakeUp();
-            std::shared_ptr<Timer> addTimerSinceNow(const std::function<void()>&task, long start, long interval, int repeat);
-            std::shared_ptr<Timer> addTimerAbsolute(const std::function<void()>&task, long start, long interval, int repeat);
-            void delTimer(const std::shared_ptr<Timer>& timer);
-            void addPendingTasks(const std::vector<std::function<void()>>&);
-            std::shared_ptr<TimerQueue> timerQueue() {return timerQueueOfEventLoop;}
+
+            std::shared_ptr<Timer>
+            addTimerSinceNow(const std::function<void()> &task, long start, long interval, int repeat);
+
+            std::shared_ptr<Timer>
+            addTimerAbsolute(const std::function<void()> &task, long start, long interval, int repeat);
+
+            void delTimer(const std::shared_ptr<Timer> &timer);
+
+            void addPendingTasks(const std::vector<std::function<void()>> &);
+
         private:
+            typedef std::vector<std::function<void()>> PendingTaskList;
+
             const pid_t threadId_;
             bool looping_;
             std::atomic_bool stop_;
             std::shared_ptr<tinyNL::net::Multiplexer> multiplexer;
-            typedef std::vector<std::function<void()>> PendingTaskList;
             PendingTaskList pendingTasks;
-            void doPendingTask();
             tinyNL::base::Mutex mutex_;
-
             int eventfd_;
             std::unique_ptr<Channel> channelUPtr;
             //every eventloop owns one timerqueue.
             std::shared_ptr<TimerQueue> timerQueueOfEventLoop;
-            void queueInLoop(const std::function<void()>& function);
-            int setUpEventFd();
+
             static void eventReadCallBack(int fd);
+
+            void doPendingTask();
+
+            int setUpEventFd();
+
+            void queueInLoop(const std::function<void()> &function);
         };
     }
 }
--- tinyNL/net/TimerQueue.h
+++ tinyNL/net/TimerQueue.h
@@ -11,43 +11,56 @@
 #include <vector>
 #include <set>
 
-namespace tinyNL{
-    namespace net{
+namespace tinyNL {
+    namespace net {
         class Channel;
+
         class Timer;
+
         class EventLoop;
-        class TimerQueue : Noncopyable{
+
+        class TimerQueue : Noncopyable {
         public:
             //one timerqueue should be associate with only one eventloop
             //it's weird that one timer queue deals with multi eventloop
-            TimerQueue(EventLoop* eventLoop);
+            TimerQueue(EventLoop *eventLoop);
+
             ~TimerQueue();
-            void addTimer(const std::shared_ptr<Timer>&);
-            void delTimer(const std::shared_ptr<Timer>&);
+
+            void addTimer(const std::shared_ptr<Timer> &);
+
+            void delTimer(const std::shared_ptr<Timer> &);
 
         private:
             //order by start time, if start time are the same, order by ptr of time obj
-            static bool cmp(const std::shared_ptr<Timer>& left, const std::shared_ptr<Timer>& right);
+            static bool cmp(const std::shared_ptr<Timer> &left, const std::shared_ptr<Timer> &right);
+
             //avoid annoying grammar checkout from clion
-            typedef decltype(cmp)* cmptype;
+            typedef decltype(cmp) *cmptype;
             int timerfd_;
-            EventLoop* loop_;
+            EventLoop *loop_;
             //timerqueue owns every timer. only modified in owner thread of eventloop
             std::set<std::shared_ptr<Timer>, cmptype> timerQueue;
             std::shared_ptr<Channel> channelPtr_;
-            void readCallBack();
-            void expireLogic();
-            std::vector<std::function<void()>> insertRepeatableTimerAndGenePendingTasks(std::vector<std::shared_ptr<Timer>> &expiredTimerList);
             std::function<void()> rcb;
-            std::vector<std::shared_ptr<Timer>> getExpiredTimersAndRemoveThemFromTimerQueue();
+
             void setNextTimeAlarm(bool on, long millionSeconds);
-            void addTimerInEventLoop(const std::shared_ptr<Timer>&);
+
+            void addTimerInEventLoop(const std::shared_ptr<Timer> &);
 
             void delTimerInEventLoop(const std::shared_ptr<Timer> &);
+
+            void readCallBack();
+
+            void expireLogic();
+
+            std::vector<std::function<void()>>
+            insertRepeatableTimerAndGenePendingTasks(std::vector<std::shared_ptr<Timer>> &expiredTimerList);
+
+            std::vector<std::shared_ptr<Timer>> getExpiredTimersAndRemoveThemFromTimerQueue();
         };
     }
 }
-
 
 
 #endif //TINYNL_TIMERQUEUE_H
