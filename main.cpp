58a32c01be0d1511d8f005e868a397665c874542
Nov11 <Nov11@users.noreply.github.com>
2017-05-18 23:42:21 +0800

    Delete main.cpp

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <tinyNL/net/CurrentThread.h>
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    pid_t ret = tinyNL::net::CurrentThread::tid();
-    std::cout << ret << std::endl;
-    return 0;
-}
